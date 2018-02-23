598ef3a4830ebf6a490186714a9a7637367c0754
Cédric Dumay <cedric.dumay@gmail.com>
2018-02-23 12:02:27 +0100

    1st working version

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# cdumay-errors-rs
+# cdumay_errors
+
+cdumay_errors is a basic library used to standardize errors and to serialize them into json using [serde_json](https://docs.serde.rs/serde/).
+
+## Quickstart
+
+```toml
+[dependencies]
+cdumay_errors = "0.1"
+```
+
+```rust
+extern crate cdumay_errors;
+extern crate serde_json;
+
+use cdumay_errors::Error;
+use serde_json::{Map, Value, to_string_pretty};
+
+let mut context: Map<String, Value> = Map::new();
+context.insert("url".to_string(), Value::from("https://example.dumay"));
+
+let err = Error::new("Not Found".to_string(), Some(404), Some("NotFound".to_string()), Some(context));
+println!("{}", to_string_pretty(&err).unwrap());
+```
+
+## Features
+
+- [cdumay-result](https://github.com/cdumay/cdumay-result-rs): A library to serialize and deserialize result using JSON.
+
+## Project Links
+
+- Issues: https://github.com/cdumay/cdumay-errors-rs/issues
+- Documentation: 
+
+## License
+
+MIT License
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,54 @@
+#[deny(warnings)]
 extern crate serde;
 extern crate serde_json;
 
 #[macro_use]
-extern crate log;
-
-#[macro_use]
 extern crate serde_derive;
 
+#[cfg(feature = "cdumay-result")]
+extern crate cdumay_result;
+
+#[derive(Serialize, Deserialize)]
+pub struct Error {
+    code: u16,
+    extra: serde_json::Map<String, serde_json::Value>,
+    message: String,
+    msgid: Option<String>,
+}
+
+impl Error {
+    pub fn new(message: String, code: Option<u16>, msgid: Option<String>, extra: Option<serde_json::Map<String, serde_json::Value>>) -> Error {
+        Error {
+            code: code.unwrap_or(1),
+            extra: extra.unwrap_or(serde_json::Map::new()),
+            message: message,
+            msgid: msgid,
+        }
+    }
+    pub fn code(&self) -> u16 { self.code }
+    pub fn extra(&self) -> serde_json::Map<String, serde_json::Value> { self.extra.clone() }
+    pub fn message(&self) -> String { self.message.clone() }
+    pub fn msgid(&self) -> Option<String> { self.msgid.clone() }
+}
+
+impl std::fmt::Debug for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "Error<code={}, message={}", self.code(), self.message())
+    }
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "Error {}: {}", self.code(), self.message())
+    }
+}
+
+#[cfg(feature = "cdumay-result")]
+use cdumay_result::BaseResult;
+
+#[cfg(feature = "cdumay-result")]
+impl From<cdumay_result::ExecutionResult> for Error {
+    fn from(result: cdumay_result::ExecutionResult) -> Error {
+        Error::new(result.stderr(), Some(result.retcode()), None, Some(result.retval()))
+    }
+}

f25b1bad925342b381248b3d831c77c5d359c78f
Cédric Dumay <cedric.dumay@gmail.com>
2019-01-14 11:57:16 +0100

    devel: removed builder

--- src/lib.rs
+++ src/lib.rs
@@ -5,92 +5,29 @@ extern crate serde_value;
 #[macro_use]
 extern crate serde_derive;
 
-use std::collections::HashMap;
-
-pub struct ErrorBuilder {
-    code: u16,
-    extra: Option<HashMap<String, serde_value::Value>>,
-    message: Option<String>,
-    msgid: String,
-    stack: Option<String>,
-}
-
-impl ErrorBuilder {
-    pub fn new(msgid: &str) -> ErrorBuilder {
-        ErrorBuilder {
-            code: 500,
-            extra: None,
-            message: None,
-            msgid: msgid.to_string(),
-            stack: None,
-        }
-    }
-    pub fn code(mut self, code: u16) -> ErrorBuilder {
-        self.code = code;
-        self
-    }
-    pub fn extra(mut self, extra: HashMap<String, serde_value::Value>) -> ErrorBuilder {
-        self.extra = Some(extra);
-        self
-    }
-    pub fn message(mut self, message: String) -> ErrorBuilder {
-        self.message = Some(message);
-        self
-    }
-    pub fn stack(mut self, stack: String) -> ErrorBuilder {
-        self.stack = Some(stack);
-        self
-    }
-    pub fn build(self) -> Error {
-        Error {
-            code: self.code,
-            extra: self.extra,
-            message: self.message.unwrap_or("".to_string()),
-            msgid: self.msgid,
-            stack: self.stack,
-        }
-    }
-}
-
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Error {
     code: u16,
     #[serde(skip_serializing_if = "Option::is_none")]
-    extra: Option<HashMap<String, serde_value::Value>>,
+    extra: Option<std::collections::HashMap<String, serde_value::Value>>,
     message: String,
     msgid: String,
     #[serde(skip_serializing_if = "Option::is_none")]
     stack: Option<String>,
 }
 
-impl Error {
-    pub fn code(&self) -> &u16 {
-        &self.code
-    }
-    pub fn code_mut(&mut self) -> &mut u16 { &mut self.code }
-
-    pub fn extra(&self) -> &Option<HashMap<String, serde_value::Value>> {
-        &self.extra
-    }
-    pub fn extra_mut(&mut self) -> &mut Option<HashMap<String, serde_value::Value>> {
-        &mut self.extra
-    }
-
-    pub fn message(&self) -> &String {
-        &self.message
-    }
-    pub fn message_mut(&mut self) -> &mut String { &mut self.message }
-
-    pub fn msgid(&self) -> &String { &self.msgid }
-    pub fn msgid_mut(&mut self) -> &mut String { &mut self.msgid }
-
-    pub fn stack(&self) -> &Option<String> {
-        &self.stack
-    }
-    pub fn stack_mut(&mut self) -> &mut Option<String> {
-        &mut self.stack
-    }
+pub trait ErrorProperties {
+    fn code(&self) -> &u16;
+    fn code_mut(&mut self) -> &mut u16;
+    fn extra(&self) -> &Option<std::collections::HashMap<String, serde_value::Value>>;
+    fn extra_mut(&mut self) -> &mut Option<std::collections::HashMap<String, serde_value::Value>>;
+    fn message(&self) -> &String;
+    fn message_mut(&mut self) -> &mut String;
+    fn msgid(&self) -> &String;
+    fn msgid_mut(&mut self) -> &mut String;
+    fn stack(&self) -> &Option<String>;
+    fn stack_mut(&mut self) -> &mut Option<String>;
 }
 
 impl Default for Error {
@@ -105,22 +42,39 @@ impl Default for Error {
     }
 }
 
+impl ErrorProperties for Error {
+    fn code(&self) -> &u16 { &self.code }
+    fn code_mut(&mut self) -> &mut u16 { &mut self.code }
+
+    fn extra(&self) -> &Option<std::collections::HashMap<String, serde_value::Value>> { &self.extra }
+    fn extra_mut(&mut self) -> &mut Option<std::collections::HashMap<String, serde_value::Value>> { &mut self.extra }
+
+    fn message(&self) -> &String { &self.message }
+    fn message_mut(&mut self) -> &mut String { &mut self.message }
+
+    fn msgid(&self) -> &String { &self.msgid }
+    fn msgid_mut(&mut self) -> &mut String { &mut self.msgid }
+
+    fn stack(&self) -> &Option<String> { &self.stack }
+    fn stack_mut(&mut self) -> &mut Option<String> { &mut self.stack }
+}
+
 
 impl std::error::Error for Error {
     fn description(&self) -> &str {
-        self.message.as_str()
+        self.message().as_str()
     }
 }
 
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "{}: {}", self.msgid, self.message)
+        write!(f, "{}: {}", self.msgid(), self.message())
     }
 }
 
 
 impl From<std::option::NoneError> for Error {
-    fn from(err: std::option::NoneError) -> Error {
+    fn from(_err: std::option::NoneError) -> Error {
         Error {
             msgid: "Err-08414".to_string(),
             message: "Not Found".to_string(),
